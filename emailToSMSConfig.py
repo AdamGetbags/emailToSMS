d6def5cd2adc074b6564206df52de34ed0193172
Adam Getbags <91522359+AdamGetbags@users.noreply.github.com>
2023-12-19 18:13:07 -0800

    Create emailToSMSConfig.py

---
+++ emailToSMSConfig.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Email to SMS Gateway - config
+@author: adam getbags
+"""
+
+senderEmail = 'email@address.com'
+gatewayAddress = '1112224444@vmobl.com'
+appKey = 'abcd efgh ijkl mnop'
